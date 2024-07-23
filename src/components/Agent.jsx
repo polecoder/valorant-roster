ef884fb3e773937627a972b6ebea28cbef669d56
Mauro Polenta <maurop4502@gmail.com>
2024-07-23 12:04:50 -0300

    fix problemas con overflow imagenes

--- src/components/Agent.jsx
+++ src/components/Agent.jsx
@@ -34,13 +34,7 @@ function Agent({ agent }) {
         alt={agent.displayName}
         className="hover-image w-[320px] max-w-fit scale-[115%] transition-transform duration-300"
       />
-      <h3
-        className="hover-text before:border-primary-red absolute bottom-0 left-1/2 z-10 w-[110px] -translate-x-1/2 -translate-y-1/2 text-center text-2xl text-white transition-transform duration-300 before:absolute before:left-1/2 before:top-1/2 before:z-[-1] before:w-full before:-translate-x-1/2 before:-translate-y-1/2 before:border-[0.4em] before:border-y-[0.6em] before:border-solid before:border-l-transparent before:border-r-transparent"
-        /* style={{
-          backgroundImage:
-            "url('https://s2.svgbox.net/pen-brushes.svg?ic=brush-4&color=000')",
-        }} */
-      >
+      <h3 className="hover-text absolute bottom-0 left-1/2 z-10 w-[110px] -translate-x-1/2 -translate-y-1/2 text-center text-2xl text-white transition-transform duration-300 before:absolute before:left-1/2 before:top-1/2 before:z-[-1] before:w-full before:-translate-x-1/2 before:-translate-y-1/2 before:border-[0.4em] before:border-y-[0.6em] before:border-solid before:border-primary-red before:border-l-transparent before:border-r-transparent">
         {agent.displayName}
       </h3>
     </div>
--- src/components/AgentList.jsx
+++ src/components/AgentList.jsx
@@ -53,8 +53,15 @@ function AgentList() {
 
   return (
     <div>
-      <h2 className="mb-8 text-center">AgentList</h2>
-      <div className="flex flex-col gap-y-24">
+      <div className="mb-8 p-4">
+        <h2 className="mb-8 text-center">AgentList</h2>
+        <p>
+          Discover the unique and dynamic agents of Valorant, each equipped with
+          distinct abilities and playstyles. Explore their backgrounds, special
+          skills, and roles to gain a competitive edge in your next match.
+        </p>
+      </div>
+      <div className="flex h-fit flex-col gap-y-24 overflow-x-hidden pb-16">
         {agents.length > 0 ? (
           agents.map((agent) => <Agent key={agent.uuid} agent={agent} />)
         ) : (
