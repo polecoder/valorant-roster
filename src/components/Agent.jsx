a7a8f87bd35a1f8152caf4efa63a1854b4729eb0
Mauro Polenta <maurop4502@gmail.com>
2024-08-05 22:58:44 -0300

    + responsive agent list

--- src/components/Agent.jsx
+++ src/components/Agent.jsx
@@ -29,13 +29,13 @@ import PropTypes from "prop-types";
 function Agent({ agent }) {
   return (
     <div className="relative mx-auto w-fit">
-      <div className="hover-area absolute left-1/2 top-1/2 h-[290px] w-[150px] -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="hover-area absolute left-1/2 top-1/2 h-[190px] w-[150px] -translate-x-1/2 -translate-y-1/2 sm:h-[380px] sm:w-[200px]"></div>
       <img
         src={agent.fullPortrait}
         alt={agent.displayName}
-        className="hover-image w-[320px] max-w-fit scale-[115%] transition-transform duration-300"
+        className="hover-image w-[320px] max-w-fit scale-[115%] transition-transform duration-300 sm:w-[375px]"
       />
-      <h3 className="hover-text absolute bottom-0 left-1/2 z-10 w-[110px] -translate-x-1/2 -translate-y-1/2 text-center text-2xl uppercase text-white transition-transform duration-300 before:absolute before:left-1/2 before:top-[47%] before:z-[-1] before:w-full before:-translate-x-1/2 before:-translate-y-1/2 before:border-[8px] before:border-y-[16px] before:border-solid before:border-primary-red before:border-l-transparent before:border-r-transparent">
+      <h3 className="hover-text absolute bottom-0 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 px-7 text-center text-2xl uppercase text-white transition-transform duration-300 before:absolute before:left-1/2 before:top-[47%] before:z-[-1] before:w-full before:-translate-x-1/2 before:-translate-y-1/2 before:border-[8px] before:border-y-[16px] before:border-solid before:border-primary-red before:border-l-transparent before:border-r-transparent sm:px-9 sm:text-3xl sm:before:top-[51%] sm:before:border-y-[18px]">
         {agent.displayName}
       </h3>
     </div>
--- src/components/AgentList.jsx
+++ src/components/AgentList.jsx
@@ -11,7 +11,6 @@ function AgentList() {
       .then((response) => response.json())
       .then((data) => {
         const agentsRawData = data.data;
-        console.log(agentsRawData);
 
         const agentsArray = [];
         agentsRawData.forEach((agent) => {
@@ -53,12 +52,16 @@ function AgentList() {
 
   return (
     <>
-      <h2 className="pb-4 pt-6 text-center text-5xl uppercase">Agents</h2>
-      <div className="flex h-fit flex-col gap-y-24 overflow-x-hidden pb-16">
+      <h2 className="pb-4 pt-6 text-center text-5xl uppercase md:text-6xl">
+        Agents
+      </h2>
+      <div className="mx-auto flex h-fit flex-col gap-y-24 overflow-x-hidden pb-16 pt-8 md:grid md:max-w-fit md:grid-cols-2 md:gap-y-48 md:pb-32 md:pt-12 lg:grid-cols-3">
         {agents.length > 0 ? (
           agents.map((agent) => <Agent key={agent.uuid} agent={agent} />)
         ) : (
-          <p>Loading...</p>
+          <p className="text-center font-lato text-xl text-primary-red md:text-2xl">
+            Loading...
+          </p>
         )}
       </div>
     </>
