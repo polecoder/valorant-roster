a58046d104142b54e27bc126f7a2fb6cda1ea0eb
polecoder <maurop4502@gmail.com>
2024-11-21 10:03:08 -0300

    + AgentDetailsHero design mod for 1024px+

--- src/components/AgentDetails/AgentDetailsHero.jsx
+++ src/components/AgentDetails/AgentDetailsHero.jsx
@@ -8,7 +8,7 @@ function AgentDetailsHero({ agent }) {
   };
 
   return (
-    <section className="relative mx-auto flex max-w-[500px] items-center overflow-hidden px-4 py-8 xs:h-[350px] xs:px-8 md:h-[400px] md:max-w-[1000px] lg:px-12">
+    <section className="relative mx-auto flex max-w-[500px] items-center overflow-hidden px-4 py-8 xs:h-[350px] xs:px-8 md:h-[400px] md:max-w-[1000px] lg:h-[450px] lg:px-12">
       <div className="w-[150px] xs:w-[200px] md:w-[350px] lg:w-[450px]">
         <h2 className="mb-4 font-tungsten text-5xl uppercase md:text-6xl">
           {agent.displayName}
@@ -16,11 +16,11 @@ function AgentDetailsHero({ agent }) {
         <p className="md:text-base">{agent.description}</p>
       </div>
       <div
-        className="agent_bg-effect absolute left-3/4 top-1/2 h-[250px] w-[200px] -translate-x-1/2 -translate-y-1/2 xs:h-[325px] xs:w-[250px] md:top-2/3 md:h-[500px] md:w-[350px] lg:top-3/4 lg:h-[600px] lg:w-[450px]"
+        className="agent_bg-effect absolute left-3/4 top-1/2 h-[250px] w-[200px] -translate-x-1/2 -translate-y-1/2 xs:h-[325px] xs:w-[250px] md:top-2/3 md:h-[500px] md:w-[350px] lg:h-[600px] lg:w-[450px]"
         style={agentBackgroundStyle}
       ></div>
       <img
-        className="absolute left-3/4 top-1/2 w-[300px] -translate-x-1/2 -translate-y-1/2 xs:w-[350px] md:top-2/3 md:w-[650px] lg:top-3/4 lg:w-[750px]"
+        className="absolute left-3/4 top-1/2 w-[300px] -translate-x-1/2 -translate-y-1/2 xs:w-[350px] md:top-2/3 md:w-[650px] lg:w-[750px]"
         src={agent.fullPortrait}
         alt={agent.displayName}
       />
